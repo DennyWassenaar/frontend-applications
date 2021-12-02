aed11d67f634445eb735390396fe318ebde77ff0
Denny Wassenaar <wassenaardenny@gmail.com>
2021-12-02 11:21:50 +0100

    Eerste versie eindopdracht

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
+import BarChart from './components/BarChart';
+import { data } from "./data/data.js";
 import { Routes, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,22 +9,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="visualisation" element={<Visualisation />} />
       </Routes>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
@@ -31,28 +18,26 @@ function Home() {
   return (
     <>
       <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
+        <h2>Homepagina</h2>
+        <p>Dit is de eindopdracht van frontend-apps</p>
       </main>
       <nav>
-        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+        <Link to="/visualisation">Visualisation</Link>
       </nav>
     </>
   );
 }
-
-function About() {
+function Visualisation() {
   return (
     <>
       <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
+        <h2>Dikke visualisatie ouwe, helemaal leip.</h2>
+        <BarChart data={data} />
       </main>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/visualisation">Visualisation</Link>
       </nav>
     </>
   );
